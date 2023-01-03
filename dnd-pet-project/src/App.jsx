7f86685ac137c2751724eb83e64e933b42b3891b
AlexStorzh <ktgcov57@gmail.com>
2023-01-04 00:27:30 +0200

    In a week

---
+++ dnd-pet-project/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{color: 'red', textAlign: 'center'}}>
+      Here will be site
+    </div>
+  );
+}
+
+export default App;
